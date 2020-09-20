5db728947edaafbf182c7fcfef83f9cae0b8ef5f
swasin185 <52885109+swasin185@users.noreply.github.com>
2020-09-20 21:05:31 +0700

    แก้ไข CircleOO

--- CircleOO/Circle.js
+++ CircleOO/Circle.js
@@ -1,39 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 // Class 
 var Circle = /** @class */ (function () {
     function Circle(x, y) {
@@ -42,65 +6,66 @@ var Circle = /** @class */ (function () {
         this.x = x;
         this.y = y;
         this.radius = Math.floor(Math.random() * 30) + 11;
-        this.color = Circle.colors[Math.floor(Math.random() * Circle.colors.length)];
+        this.color = Circle.COLORS[Math.floor(Math.random() * Circle.COLORS.length)];
+        this.r2 = this.radius * this.radius;
     }
-    Circle.prototype.paint = function () {
+    Circle.prototype.draw = function () {
         cx.shadowBlur = 10;
         cx.shadowColor = "Grey";
         cx.fillStyle = this.color;
         cx.beginPath();
-        cx.arc(this.x, this.y, this.radius, 0, Circle.pi2);
+        cx.arc(this.x, this.y, this.radius, 0, Circle.PI2);
         cx.closePath();
         cx.fill();
     };
     Circle.prototype.isClickIn = function (x, y) {
-        var a2 = x - this.x;
-        a2 *= a2;
-        var b2 = y - this.y;
-        b2 *= b2;
-        var c2 = this.radius * this.radius;
-        return (a2 + b2) <= c2;
+        var c2 = (x - this.x) * (x - this.x) + (y - this.y) * (y - this.y);
+        return c2 <= this.r2;
     };
-    Circle.colors = ['magenta', 'cyan', 'blue', 'green', 'yellow', 'orange', 'red'];
+    Circle.COLORS = ['magenta', 'cyan', 'blue', 'green', 'yellow', 'orange', 'red'];
     // private static cv = document.getElementById("myCanvas") as HTMLCanvasElement;
     // private static  cx = Circle.cv.getContext("2d") as CanvasRenderingContext2D;
-    Circle.pi2 = Math.PI * 2;
+    Circle.PI2 = Math.PI * 2;
     return Circle;
 }());
 // ประกาศตัวแปร Global 
 // ----------------------------------------------------------------------------
 var cv = document.getElementById("myCanvas");
 var cx = cv.getContext("2d");
-var WIDTH = cv.width;
-var HEIGHT = cv.height;
-var MID_WIDTH = cv.width / 2;
-var MID_HEIGHT = cv.height / 2;
-var data = new Array(100);
+var data = new Array(10);
 var size = 0;
 // การทำงานเริ่มต้น
 // ----------------------------------------------------------------------------
-calculate();
-// ฟังก์ชั่น
+// Function
 // ----------------------------------------------------------------------------
-function calculate() {
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            return [2 /*return*/];
-        });
-    });
-}
+// async function calculate() {
+//     paint();
+//     await new Promise(r => setTimeout(r, 10));
+// }
 function clickXY(event) {
     var x = event.offsetX;
     var y = event.offsetY;
+    //    console.log(x, y);
+    // เช็คว่าคลิกโดนวงกลมวงใดวงหนึ่งหรือไม่
     var found = -1;
     for (var i = 0; i < size && found == -1; i++) {
         if (data[i].isClickIn(x, y))
             found = i;
     }
     if (found == -1) {
+        // ถ้าคลิกไม่โดนวงกลม ให้เพิ่มวงใหม่
+        // ถ้าขนาดอาเรย์ เต็มแล้วให้ขยายไปยังอาเรย์ตัวใหม่ ใหญขึ้น 2 เท่า
+        if (size == data.length) {
+            var newData = new Array(data.length * 2);
+            for (var i = 0; i < size; i++)
+                newData[i] = data[i];
+            data = newData;
+        }
+        // เพิ่มวงกลมวงใหม่
         data[size++] = new Circle(x, y);
     }
     else {
+        // ลบวงกลมที่คลิกโดน
         for (var i = found + 1; i < size; i++)
             data[i - 1] = data[i];
         size--;
@@ -110,5 +75,5 @@ function clickXY(event) {
 function paint() {
     cx.clearRect(0, 0, cv.width, cv.height);
     for (var i = 0; i < size; i++)
-        data[i].paint();
+        data[i].draw();
 }
--- CircleOO/Circle.ts
+++ CircleOO/Circle.ts
@@ -1,36 +1,34 @@
 // Class 
 class Circle {
-    private static colors = ['magenta', 'cyan', 'blue', 'green', 'yellow', 'orange', 'red'];
+    static COLORS = ['magenta', 'cyan', 'blue', 'green', 'yellow', 'orange', 'red'];
     // private static cv = document.getElementById("myCanvas") as HTMLCanvasElement;
     // private static  cx = Circle.cv.getContext("2d") as CanvasRenderingContext2D;
-    private static pi2 = Math.PI * 2;
+    static PI2 = Math.PI * 2;
 
-    private x: number;
-    private y: number;
-    private radius: number = 10;
-    private color: string = 'Grey';
+    x: number;
+    y: number;
+    radius: number = 10;
+    r2: number;
+    color: string = 'Grey';
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
         this.radius = Math.floor(Math.random() * 30) + 11;
-        this.color = Circle.colors[Math.floor(Math.random() * Circle.colors.length)];
+        this.color = Circle.COLORS[Math.floor(Math.random() * Circle.COLORS.length)];
+        this.r2 = this.radius * this.radius;
     }
-    public paint() {
+    draw() {
         cx.shadowBlur = 10;
         cx.shadowColor = "Grey";
         cx.fillStyle = this.color;
         cx.beginPath()
-        cx.arc(this.x, this.y, this.radius, 0, Circle.pi2);
+        cx.arc(this.x, this.y, this.radius, 0, Circle.PI2);
         cx.closePath();
         cx.fill();
     }
-    public isClickIn(x: number, y: number): boolean {
-        let a2 = x - this.x;
-        a2 *= a2;
-        let b2 = y - this.y;
-        b2 *= b2;
-        let c2 = this.radius * this.radius;
-        return (a2 + b2) <= c2;
+    isClickIn(x: number, y: number): boolean {
+        let c2 = (x - this.x) * (x - this.x) + (y - this.y) * (y - this.y);
+        return c2 <= this.r2;
     }
 }
 
@@ -39,56 +37,52 @@ class Circle {
 const cv = document.getElementById("myCanvas") as HTMLCanvasElement;
 const cx = cv.getContext("2d") as CanvasRenderingContext2D;
 
-const WIDTH = cv.width;
-const HEIGHT = cv.height;
-const MID_WIDTH = cv.width / 2;
-const MID_HEIGHT = cv.height / 2;
-
-var data: Circle[] = new Array(100);
+var data: Circle[] = new Array(10);
 var size: number = 0;
 
 // การทำงานเริ่มต้น
 // ----------------------------------------------------------------------------
-calculate();
 
-// ฟังก์ชั่น
+// Function
 // ----------------------------------------------------------------------------
-async function calculate() {
-    // paint();
-    // for (let i = 0; i < 150; i++) {
-    //     ball.y += 5;
-    //     //ball.paint();
-    //     paint();
-    //     await new Promise(r => setTimeout(r, 10));
-    // }
-}
+// async function calculate() {
+//     paint();
+//     await new Promise(r => setTimeout(r, 10));
+// }
 
 function clickXY(event: MouseEvent) {
     let x = event.offsetX;
     let y = event.offsetY;
+//    console.log(x, y);
+    // เช็คว่าคลิกโดนวงกลมวงใดวงหนึ่งหรือไม่
     let found: number = -1;
     for (let i = 0; i < size && found == -1; i++) {
         if (data[i].isClickIn(x, y))
             found = i;
     }
     if (found == -1) {
-        if (size = data.length) {
+        // ถ้าคลิกไม่โดนวงกลม ให้เพิ่มวงใหม่
+        // ถ้าขนาดอาเรย์ เต็มแล้วให้ขยายไปยังอาเรย์ตัวใหม่ ใหญขึ้น 2 เท่า
+        if (size == data.length) {
             let newData: Circle[] = new Array(data.length * 2);
             for (let i = 0; i < size; i++)
-                newData[i] = data[i]
+                newData[i] = data[i];
             data = newData;
         }
+        // เพิ่มวงกลมวงใหม่
         data[size++] = new Circle(x, y);
     } else {
+        // ลบวงกลมที่คลิกโดน
         for (let i = found + 1; i < size; i++)
-            data[i - 1] = data[i]
+            data[i - 1] = data[i];
         size--;
     }
+
     paint();
 }
 
 function paint() {
     cx.clearRect(0, 0, cv.width, cv.height);
     for (let i = 0; i < size; i++)
-        data[i].paint();
+        data[i].draw();
 }

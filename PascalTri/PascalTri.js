c75f4b9b2a0df1231b70202ff2427788b956b5a7
swasin185 <52885109+swasin185@users.noreply.github.com>
2020-09-22 17:58:12 +0700

    Start PascalTri

--- PascalTri/PascalTri.js
+++ PascalTri/PascalTri.js
@@ -34,19 +34,53 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var COLORS = ['magenta', 'cyan', 'blue', 'green', 'yellow', 'orange', 'red'];
+var PI2 = Math.PI * 2;
 // Class 
 var Ball = /** @class */ (function () {
-    function Ball() {
-        this.radius = 10;
+    function Ball(x, y) {
+        this.radius = 5; // pixel 
+        this.speed = 0; // pixel per frame
+        this.direction = 0; // radian
+        this.color = 'Grey'; // color name
+        this.x = x;
+        this.y = y;
+        this.radius = Math.floor(Math.random() * 10) + 11;
+        this.color = COLORS[Math.floor(Math.random() * COLORS.length)];
+        this.radiusPower2 = this.radius * this.radius;
     }
-    Ball.prototype.paint = function () {
-        cx.shadowBlur = 10;
-        cx.shadowColor = "Grey";
-        cx.fillStyle = "White";
+    Ball.prototype.draw = function () {
+        cx.fillStyle = this.color;
         cx.beginPath();
-        cx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        cx.arc(this.x, this.y, this.radius, 0, PI2);
         cx.closePath();
         cx.fill();
+    };
+    Ball.prototype.isClickIn = function (x, y) {
+        var c2 = (x - this.x) * (x - this.x) + (y - this.y) * (y - this.y);
+        return c2 <= this.radiusPower2;
+    };
+    Ball.prototype.move = function () {
+        var dx = Math.cos(this.direction) * this.speed;
+        var dy = Math.sin(this.direction) * this.speed;
+        this.x += dx;
+        this.y += dy;
+    };
+    Ball.prototype.reflect = function (pAngle) {
+        this.direction = 2 * pAngle - this.direction;
+    };
+    Ball.prototype.setRadius = function (radius) {
+        this.radius = radius;
+        this.radiusPower2 = radius * radius;
+    };
+    Ball.prototype.getCollideAngle = function (ball) {
+        var dx = this.x - ball.x;
+        var dy = this.y - ball.y;
+        var angle = PI2;
+        if (dx != 0) {
+            angle = Math.atan(dy / dx);
+        }
+        return angle - Math.PI / 2;
     };
     return Ball;
 }());
@@ -70,15 +104,13 @@ var WIDTH = cv.width;
 var HEIGHT = cv.height;
 var MID_WIDTH = cv.width / 2;
 var MID_HEIGHT = cv.height / 2;
-var imgData = cx.createImageData(WIDTH, HEIGHT); // width x height
-var data = imgData.data;
+// var imgData = cx.createImageData(WIDTH, HEIGHT); // width x height
+// var data = imgData.data;
 var n = 10;
-var ball = new Ball();
-ball.x = WIDTH / 2;
-ball.y = 20;
+var ball = new Ball(WIDTH / 2, 2);
+paint();
 // การทำงานเริ่มต้น
 // ----------------------------------------------------------------------------
-calculate();
 // ฟังก์ชั่น
 // ----------------------------------------------------------------------------
 function calculate() {
@@ -87,15 +119,13 @@ function calculate() {
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    paint();
                     i = 0;
                     _a.label = 1;
                 case 1:
                     if (!(i < 150)) return [3 /*break*/, 4];
-                    ball.y += 5;
-                    //ball.paint();
+                    ball.y += 2.5;
                     paint();
-                    return [4 /*yield*/, new Promise(function (r) { return setTimeout(r, 10); })];
+                    return [4 /*yield*/, new Promise(function (r) { return setTimeout(r, 5); })];
                 case 2:
                     _a.sent();
                     _a.label = 3;
@@ -107,51 +137,44 @@ function calculate() {
         });
     });
 }
-function clickXY(event) {
-    ball.x = event.offsetX;
-    ball.y = event.offsetY;
-    paint();
-}
 function paint() {
     n = Number(boardSize.value);
     cx.clearRect(0, 0, cv.width, cv.height);
     var x = MID_WIDTH;
     var y = 10;
     var size = 50;
-    cx.fillStyle = "Blue";
+    cx.fillStyle = 'black';
     cx.shadowBlur = 10;
-    cx.shadowColor = "Grey";
+    cx.shadowColor = 'Grey';
     var mid = size / 2;
     for (var i = 0; i < n; i++) {
         x = MID_WIDTH - (i * mid);
         cx.beginPath();
-        cx.arc(x - mid, y + size, 5, 0, 2 * Math.PI);
+        cx.arc(x - mid, y + size, 5, 0, PI2);
         cx.closePath();
         cx.fill();
         for (var j = 0; j <= i; j++) {
             // cx.beginPath();
-            // cx.fillStyle = "Orange";
-            // cx.shadowBlur = 0;
             // cx.moveTo(x, y);
             // cx.lineTo(x + mid, y + size);
             // cx.lineTo(x - mid, y + size);
             // cx.closePath();
-            // cx.fill();
+            // cx.stroke();
             cx.beginPath();
-            cx.arc(x + mid, y + size, 5, 0, 2 * Math.PI);
+            cx.arc(x + mid, y + size, 5, 0, PI2);
             cx.closePath();
             cx.fill();
-            // cx.beginPath();
-            // cx.moveTo(x - mid, y + size);
-            // cx.lineTo(x - mid, y + mid - 5);
-            // cx.lineTo(x, y);
-            // cx.moveTo(x + mid, y + size);
-            // cx.lineTo(x + mid, y + mid - 5);
-            // cx.lineTo(x, y);
-            // cx.stroke();
+            cx.beginPath();
+            cx.moveTo(x - mid, y + size);
+            cx.lineTo(x - mid, y + mid - 5);
+            cx.lineTo(x, y);
+            cx.moveTo(x + mid, y + size);
+            cx.lineTo(x + mid, y + mid - 5);
+            cx.lineTo(x, y);
+            cx.stroke();
             x += size;
         }
         y += size;
     }
-    ball.paint();
+    ball.draw();
 }
--- PascalTri/PascalTri.ts
+++ PascalTri/PascalTri.ts
@@ -1,16 +1,77 @@
+const COLORS = ['magenta', 'cyan', 'blue', 'green', 'yellow', 'orange', 'red'];
+const PI2 = Math.PI * 2;
+
 // Class 
+class Sprite {
+    x: number;              // horizontal position
+    y: number;              // vertical position
+    color: string = 'Grey'; // color name
+}
 class Ball {
-    public x: number;
-    public y: number;
-    public radius: number = 10;
-    public paint() {
-        cx.shadowBlur = 10;
-        cx.shadowColor = "Grey";
-        cx.fillStyle = "White";
+    x: number;              // horizontal position
+    y: number;              // vertical position
+    radius: number = 5;     // pixel 
+    speed: number = 0;      // pixel per frame
+    direction: number = 0;  // radian
+    color: string = 'Grey'; // color name
+    radiusPower2: number;             // radius power of 2
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+        this.radius = Math.floor(Math.random() * 10) + 11;
+        this.color = COLORS[Math.floor(Math.random() * COLORS.length)];
+        this.radiusPower2 = this.radius * this.radius;
+    }
+    draw() {
+        cx.fillStyle = this.color;
         cx.beginPath()
-        cx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        cx.arc(this.x, this.y, this.radius, 0, PI2);
         cx.closePath();
         cx.fill();
+    }
+    isClickIn(x: number, y: number): boolean {
+        let c2 = (x - this.x) * (x - this.x) + (y - this.y) * (y - this.y);
+        return c2 <= this.radiusPower2;
+    }
+    move(): void {
+        let dx = Math.cos(this.direction) * this.speed;
+        let dy = Math.sin(this.direction) * this.speed;
+        this.x += dx;
+        this.y += dy;
+    }
+    reflect(pAngle: number): void {
+        this.direction = 2 * pAngle - this.direction;
+    }
+    setRadius(radius: number): void {
+        this.radius = radius;
+        this.radiusPower2 = radius * radius;
+    }
+    getCollideAngle(ball: Ball): number {
+        let dx = this.x - ball.x;
+        let dy = this.y - ball.y;
+        let angle = PI2;
+        if (dx != 0) {
+            angle = Math.atan(dy / dx);
+        }
+        return angle - Math.PI / 2;
+    }
+    isCollided(ball: Ball): boolean {
+        if (this != ball && ball != null) {
+            let dx = this.x - ball.x;
+            let dy = this.y - ball.y;
+            let ballDistance = dx * dx + dy * dy;
+            let collideDistance = ball.radius + this.radius;
+            /* r is ratio of collide difference and ball distance */
+            let r = (collideDistance - ballDistance) / ballDistance;
+            /* if ball is overlape then seperate balls */
+            if (r > 0) {
+                this.x += dx * r;
+                this.y += dy * r;
+            }
+            return ballDistance <= collideDistance;
+        } else {
+            return false;
+        }
     }
 }
 
@@ -37,35 +98,24 @@ const HEIGHT = cv.height;
 const MID_WIDTH = cv.width / 2;
 const MID_HEIGHT = cv.height / 2;
 
-var imgData = cx.createImageData(WIDTH, HEIGHT); // width x height
-var data = imgData.data;
+// var imgData = cx.createImageData(WIDTH, HEIGHT); // width x height
+// var data = imgData.data;
 
 var n = 10;
 
-var ball = new Ball();
-ball.x = WIDTH / 2;
-ball.y = 20;
-
+var ball = new Ball(WIDTH / 2, 2);
+paint();
 // การทำงานเริ่มต้น
 // ----------------------------------------------------------------------------
-calculate();
 
 // ฟังก์ชั่น
 // ----------------------------------------------------------------------------
 async function calculate() {
-    paint();
     for (let i = 0; i < 150; i++) {
-        ball.y += 5;
-        //ball.paint();
+        ball.y += 2.5;
         paint();
-        await new Promise(r => setTimeout(r, 10));
+        await new Promise(r => setTimeout(r, 5));
     }
-}
-
-function clickXY(event: MouseEvent) {
-    ball.x = event.offsetX;
-    ball.y = event.offsetY;
-    paint();
 }
 
 function paint() {
@@ -74,42 +124,43 @@ function paint() {
     let x = MID_WIDTH;
     let y = 10;
     let size = 50;
-    cx.fillStyle = "Blue";
+    cx.fillStyle = 'black';
     cx.shadowBlur = 10;
-    cx.shadowColor = "Grey";
+    cx.shadowColor = 'Grey';
     let mid = size / 2;
     for (let i = 0; i < n; i++) {
         x = MID_WIDTH - (i * mid);
         cx.beginPath();
-        cx.arc(x - mid, y + size, 5, 0, 2 * Math.PI);
+        cx.arc(x - mid, y + size, 5, 0, PI2);
         cx.closePath();
         cx.fill();
+
         for (let j = 0; j <= i; j++) {
             // cx.beginPath();
-            // cx.fillStyle = "Orange";
-            // cx.shadowBlur = 0;
             // cx.moveTo(x, y);
             // cx.lineTo(x + mid, y + size);
             // cx.lineTo(x - mid, y + size);
             // cx.closePath();
-            // cx.fill();
+            // cx.stroke();
 
             cx.beginPath()
-            cx.arc(x + mid, y + size, 5, 0, 2 * Math.PI);
+            cx.arc(x + mid, y + size, 5, 0, PI2);
             cx.closePath();
             cx.fill();
-            // cx.beginPath();
-            // cx.moveTo(x - mid, y + size);
-            // cx.lineTo(x - mid, y + mid - 5);
-            // cx.lineTo(x, y);
-            // cx.moveTo(x + mid, y + size);
-            // cx.lineTo(x + mid, y + mid - 5);
-            // cx.lineTo(x, y);
-            // cx.stroke();
+
+            cx.beginPath();
+            cx.moveTo(x - mid, y + size);
+            cx.lineTo(x - mid, y + mid - 5);
+            cx.lineTo(x, y);
+            cx.moveTo(x + mid, y + size);
+            cx.lineTo(x + mid, y + mid - 5);
+            cx.lineTo(x, y);
+            cx.stroke();
+
             x += size;
 
         }
         y += size;
     }
-    ball.paint();
+    ball.draw();
 }

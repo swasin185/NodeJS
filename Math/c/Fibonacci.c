043fa67ac92652bd4ca3506743cfc6a22b5bd925
user <user@aus-lenovo>
2022-02-25 20:12:18 +0700

    fibo

--- Math/c/Fibonacci.c
+++ Math/c/Fibonacci.c
@@ -3,12 +3,12 @@
 int main() {
     int a = 0;
     int b = 1;
-    printf("%.30f\n", 0.1 + 0.2);
+    printf("%.17f\n", 0.1 + 0.2);
     for (int i=1; i<=34; i++) {
         b += a;
         a = b - a;
-        printf("%d\t%d\t%d\t:\t%.30f\n", i, a, b, (double) b / a);
-        printf("\t\t\t:\t%.20f\n", round(1E15 * (double) b / a)/1E15);
+        printf("%d\t%d\t%d\t:\t%.17f\n", i, a, b, (double) b / a);
+        printf("\t\t\t:\t%.15f\n", round(1E15 * (double) b / a)/1E15);
     }
     return 0;
 }

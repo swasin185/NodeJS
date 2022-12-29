64039865f82ad0d7bac03c63bc18592ca5a5e85c
user <user@v55t-15ARE>
2022-12-29 19:53:39 +0700

    clean kh2020 kh-panwa

--- kh-panwa/server/reports.js
+++
@@ -1,6 +0,0 @@
-export const reportList = [
-    { id: 'rep-0001', name: 'Report List', group: 'admin', param: '', desc: 'All reports information in thie file, report.js', ref: '' },
-    { id: 'rep-0002', name: 'Session List', group: 'admin', param: '', desc: 'Current session connection', ref: '' },
-    { id: 'rep-0003', name: 'User List', group: 'admin', param: '', desc: 'All users information in DB', ref: '' },
-    { id: 'rep-0004', name: 'Permission ', group: 'admin', param: '', desc: 'All users permission in DB', ref: '' }
-]
